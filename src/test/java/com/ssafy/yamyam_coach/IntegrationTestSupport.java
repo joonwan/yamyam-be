5b8dfc0c23680224c0d2ecd175a1113d5d1af766
joonwan1024 <joownan1024@naver.com>
2025-12-09 15:10:23 +0900

    fix: container 중지 문재 해결

--- src/test/java/com/ssafy/yamyam_coach/IntegrationTestSupport.java
+++ src/test/java/com/ssafy/yamyam_coach/IntegrationTestSupport.java
@@ -5,22 +5,23 @@ import org.springframework.boot.testcontainers.service.connection.ServiceConnect
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
 public abstract class IntegrationTestSupport {
 
-    @Container
     @ServiceConnection
-    static final MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
-            .withDatabaseName("yumyum_coach")
-            .withUsername("root")
-            .withPassword("1234")
-            .withInitScript("schema.sql")
-            .withReuse(true);
+    static final MySQLContainer<?> mysql;
+
+    static {
+        mysql = new MySQLContainer<>("mysql:8.0")
+                .withDatabaseName("yumyum_coach")
+                .withUsername("root")
+                .withPassword("1234")
+                .withInitScript("schema.sql");
+
+        mysql.start();
+    }
 
 }
